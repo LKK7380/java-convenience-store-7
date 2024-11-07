d489143bfd1d512365b10c38ed52e5ae1f0f8b34
Lkk7380 <dlrlrhkd@naver.com>
2024-11-07 17:06:53 +0900

    feat: Product 클래스에 가격 필드 추가

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -2,12 +2,25 @@ package store.domain;
 
 public class Product {
     private final String name;
+    private final int price;
 
     public Product(String name, int price, int quantity, String promotionName) {
+        validatePrice(price);
         this.name = name;
+        this.price = price;
+    }
+
+    private void validatePrice(int price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("[ERROR] 가격은 0 보다 커야 합니다.");
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ProductTest {
     @Test
-    @DisplayName("상품을 생성한다")
+    @DisplayName("상품 생성")
     void createProduct() {
         String name = "콜라";
         int price = 1000;
@@ -18,5 +19,14 @@ class ProductTest {
 
         assertThat(product).isNotNull();
         assertThat(product.getName()).isEqualTo(name);
+        assertThat(product.getPrice()).isEqualTo(price);
+    }
+
+    @Test
+    @DisplayName("상품 가격이 0 이하이면 예외가 발생한다")
+    void validatePrice() {
+        assertThatThrownBy(() -> new Product("콜라", 0, 10, "탄산2+1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 가격은 0 보다 커야 합니다.");
     }
 }
