7bd9e9e2d20a68a9fb798340f777296452a6afae
Lkk7380 <dlrlrhkd@naver.com>
2024-11-08 22:51:31 +0900

    feat: MembershipDiscount 클래스 할인 및 최대 할인 한도 구현

--- src/main/java/store/service/discount/MembershipDiscount.java
+++ src/main/java/store/service/discount/MembershipDiscount.java
@@ -1,6 +1,8 @@
 package store.service.discount;
 
 public class MembershipDiscount {
-    public int calculate(int i) {
+    public int calculate(int price) {
+        int discountAmount = (int) (price * 0.3);
+        return Math.min(discountAmount, 8000);
     }
 }
