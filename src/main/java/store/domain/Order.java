321e1ff20a8da4b50bfecd18de4b5bca6aacbc45
Lkk7380 <dlrlrhkd@naver.com>
2024-11-08 17:52:06 +0900

    feat: Order 클래스 유효성 검증 로직 구현

--- src/main/java/store/domain/Order.java
+++ src/main/java/store/domain/Order.java
@@ -5,8 +5,25 @@ public class Order {
     private final int quantity;
 
     public Order(String productName, int quantity) {
+        validateName(productName);
+        validateQuantity(quantity);
         this.productName = productName;
         this.quantity = quantity;
+    }
+
+    private void validateName(String productName) {
+        if (productName == null) {
+            throw new IllegalArgumentException("[ERROR] 상품명은 null일 수 없습니다.");
+        }
+        if (productName.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 상품명은 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 주문 수량은 1개 이상이어야 합니다.");
+        }
     }
 
     public String getProductName() {
