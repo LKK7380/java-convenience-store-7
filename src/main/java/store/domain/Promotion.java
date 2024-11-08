9290eb4f5bf425c04af965b9826f166ceb21c6a6
Lkk7380 <dlrlrhkd@naver.com>
2024-11-08 14:46:00 +0900

    feat: Promotion 클래스 구현

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -9,10 +9,24 @@ public class Promotion {
     private final int requiredQuantity;
 
     public Promotion(String name, LocalDateTime startDate, LocalDateTime endDate, int requiredQuantity) {
+        validateDate(startDate, endDate);
+        validateRequiredQuantity(requiredQuantity);
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
         this.requiredQuantity = requiredQuantity;
+    }
+
+    private void validateDate(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("[ERROR] 시작일이 종료일보다 늦을 수 없습니다.");
+        }
+    }
+
+    private void validateRequiredQuantity(int requiredQuantity) {
+        if (requiredQuantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 구매 필요 수량은 1 이상이어야 합니다.");
+        }
     }
 
     public String getName() {
